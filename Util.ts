443ab50d514aea2217dcbcac3d30278e6cec2c97
pumpkye <pumpkye@gmail.com>
2019-06-03 18:19:26 +0800

    提交误删的函数

--- Util.ts
+++ Util.ts
@@ -73,6 +73,18 @@ class Util {
     }
 
     /**
+    * 返回一个[1,num]的随机数
+    * @param num 
+    */
+    public getRandomNumber(num: number): number {
+        if (num <= 1) {
+            return num;
+        }
+        let rad = Math.floor(Math.random() * num) + 1;
+        return rad;
+    }
+
+    /**
      * 获取阵容触发的职业和种族buff
      * @param baseIdList 阵容的baseId列表，可重复
      */
